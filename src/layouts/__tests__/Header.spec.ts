75e6edac6defe9652887b94c685a8e2b262a9494
Danny Murphy <Danny.Murphy@decision.tech>
2021-04-28 17:44:39 +0100

    Re-enable tests

--- src/layouts/__tests__/Header.spec.ts
+++ src/layouts/__tests__/Header.spec.ts
@@ -1,26 +1,26 @@
-// import Header from '@/layouts/Header.vue';
-// import { Wrapper, shallowMount } from '@vue/test-utils';
+import Header from '@/layouts/Header.vue';
+import { Wrapper, shallowMount } from '@vue/test-utils';
 
-// describe('Header', () => {
-//   let wrapper: Wrapper<any>;
+describe('Header', () => {
+  let wrapper: Wrapper<any>;
 
-//   const mountComponent = () => {
-//     wrapper = shallowMount(Header);
-//   };
+  const mountComponent = () => {
+    wrapper = shallowMount(Header);
+  };
 
-//   beforeEach(() => {
-//     mountComponent();
-//   });
+  beforeEach(() => {
+    mountComponent();
+  });
 
-//   it('THEN renders correctly', () => {
-//     expect(wrapper).toMatchSnapshot();
-//   });
+  it('THEN renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-//   it('THEN renders the logo', () => {
-//     expect(wrapper.find('.header__logo').exists()).toBe(true);
-//   });
+  it('THEN renders the logo', () => {
+    expect(wrapper.find('.header__logo').exists()).toBe(true);
+  });
 
-//   it('THEN a contact component', () => {
-//     expect(wrapper.find('.header__contact').exists()).toBe(true);
-//   });
-// });
+  it('THEN a contact component', () => {
+    expect(wrapper.find('.header__contact').exists()).toBe(true);
+  });
+});
