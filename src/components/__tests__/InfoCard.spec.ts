5d70af8e98246067fcba2335e41bf116e596fec6
Danny Murphy <Danny.Murphy@decision.tech>
2021-05-03 15:40:22 +0100

    Tweaks following reviewing PR

--- src/components/__tests__/InfoCard.spec.ts
+++ src/components/__tests__/InfoCard.spec.ts
@@ -34,10 +34,6 @@ describe('InfoCard', () => {
         mountComponent();
       });
 
-      it('THEN renders correctly', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
       it('THEN renders the icon', () => {
         expect(wrapper.findComponent(FontAwesomeIcon).exists()).toBe(true);
       });
